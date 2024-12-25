a73e0e4eb187fc48f3d71627eb016964c792b501
WireWhiz <eli@knisley.info>
2024-12-24 19:43:46 -0500

    assetServer and editor now also have install components

--- src/assetServer/InstallDB.cmake
+++ src/assetServer/InstallDB.cmake
@@ -1,10 +1,10 @@
 
 # CMAKE_CURRENT_SOURCE_DIR doesn't work here because the script is moved
 set(SOURCE ${CMAKE_SOURCE_DIR}/src/assetServer/database/data.db)
-SET(DEST ${CMAKE_INSTALL_PREFIX}/database/data.db)
+SET(DEST ${CMAKE_INSTALL_PREFIX}/database)
 
 message("Install prefix: ${CMAKE_INSTALL_PREFIX}")
-if(${SOURCE} IS_NEWER_THAN ${DEST})
+if(${SOURCE} IS_NEWER_THAN ${DEST}/data.db)
     message("resetting database")
     file(COPY ${SOURCE} DESTINATION ${DEST})
 endif()
